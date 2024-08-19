ed6b180c877e84ed22262e8081bcd08522fe1b65
don <roguethegreat8090@gmail.com>
2024-08-18 22:31:58 -0400

    c update

--- g.c
+++ g.c
@@ -4,14 +4,32 @@
 
 int main()
 {
-    int i;
     int j[2370];
     int q[395];
+    char l[200];
+    int fi = 0;
+    int se = 0;
     FILE *f = fopen("student-mat.csv" , "r");
     if (f == NULL)
     {
-        perror("FILE FAILED GO H4NG X D");
+        perror("FILE FAILED 😹😹😹");
         return -1;
     }
 
+    fgets(l, sizeof(l), f);
+
+    while(fgets(l, sizeof(l), f) != NULL)
+    {
+        char *token;
+        int c = 0;
+
+        token = strtok(l, ";");
+        while (token != NULL)
+        {
+            if(c == )
+        }
+    }
+    
+
+    return 0;
 }
